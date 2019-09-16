fa1977ef2addf0f993224db0f516019bad01342f
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-15 19:21:48 -0500

    built character card component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({ character }) {
+  
+  const {image, name, species, status, location, } = character;
+  return (
+    <div className="characterCard">
+      <img src={image} alt="character pic" />
+      <h2>{name}</h2>
+      <p>{species}</p><p>{status}</p>
+      <br/>
+      <p>Location: {location.name}</p>
+      <br/>
+      {/* <p>Origin: {origin}</p> */}
+      <h3>Episodes</h3>
+
+      {/* {props.character.episode.map(item => (
+        <div className="episodes" key={item}>
+          {item}
+        </div>
+      ))} */}
+    
+
+    </div>
+  );
 }
