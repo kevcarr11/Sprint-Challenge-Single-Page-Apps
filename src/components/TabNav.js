49f8cc061733cba2664b619079f20c5fab1a5683
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-09-16 09:32:26 -0500

    cleaned up nav component

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,37 +1,11 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
+import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
-import { Nav, NavItem } from 'reactstrap';
 
 
 // TODO: Add missing menu/tabs/nav below
 
-// Review Semantic UI Component options for nav-like UI:
-// https://react.semantic-ui.com/collections/menu/
-// https://react.semantic-ui.com/modules/tab/
-// https://react.semantic-ui.com/elements/button/
-// https://react.semantic-ui.com/collections/breadcrumb/
-
-
 export default function TabNav() {
-    // return (
-    // <div>
-    //    <Nav tabs>
-    //       <NavItem>
-    //         <NavLink to='/'>Home</NavLink>
-    //       </NavItem>
-    //       <NavItem>
-    //         <NavLink to="/characters">Characters</NavLink>
-    //       </NavItem>
-    //       <NavItem>
-    //         <NavLink to="/locations">Locations</NavLink>
-    //       </NavItem>
-    //       <NavItem>
-    //         <NavLink to="/episodes">Episodes</NavLink>
-    //       </NavItem>
-    //     </Nav> 
-    // </div>
-    // )
 
     return (
         <Menu pointing secondary>
